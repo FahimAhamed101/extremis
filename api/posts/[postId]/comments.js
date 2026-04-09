a8c30bd94ea809a6ea6a8e85c6b678b8276c6faa
fahim <fahimahamedweb@gmail.com>
2026-04-10 00:35:49 +0600

    aaddeed socm echanges

---
+++ api/posts/[postId]/comments.js
@@ -0,0 +1,22 @@
+const { forwardToBackend } = require("../../_backend");
+
+function getPostId(req, url) {
+  const fromQuery = String(req.query?.postId || "").trim();
+  if (fromQuery) {
+    return fromQuery;
+  }
+
+  const pathname = String(url?.pathname || "");
+  const match = pathname.match(/\/api\/posts\/([^/]+)\/comments\/?$/i);
+  return match?.[1] ? String(match[1]).trim() : "";
+}
+
+module.exports = async (req, res) => {
+  const protocol = String(req.headers["x-forwarded-proto"] || "https");
+  const host = String(req.headers.host || "localhost");
+  const url = new URL(req.url || "/", `${protocol}://${host}`);
+  const postId = getPostId(req, url);
+
+  req.url = `/posts/${postId}/comments${url.search}`;
+  return forwardToBackend(req, res);
+};
